7667e06e9bd55b56b4b0c028856236b16c05bfa7
maltemo <maltemo@protonmail.com>
2019-12-11 14:02:23 +0100

    Improving readability

--- assets/js/writeup.component.js
+++ assets/js/writeup.component.js
@@ -2,7 +2,7 @@ Vue.component('write-up', {
   template: `<tr>
                     <td v-text="writeup.ctf"></td>
                     <td v-text="writeup.challenge_type"></td>
-                    <td><a :href="urlMaker(writeup)" target="_blank">{{writeup.title}}</a></td>
+                    <td><a :href="urlMaker(writeup)" class="write_up_link" target="_blank">{{writeup.title}}</a></td>
                     <td v-text="writeup.points"></td>
                     <td v-text="writeup.date.toLocaleDateString()"></td>
                     <td v-text="writeup.tags"></td>
